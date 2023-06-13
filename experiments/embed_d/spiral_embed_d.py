hlinander/equivariant-posteriors

commit 3ebc5872cbd3c4c6f1a76c3999b55925bbe210bb

Update to stable nixpkgs
Fix metric order confusion in visualization
Add positional embedding for transformer

lib/models/mlp.py
@@ -15,7 +15,8 @@ class MLPClass(torch.nn.Module):
     def __init__(self, config: MLPClassConfig, data_spec: DataSpec):
         super().__init__()
         width = config.width
-        self.mlp1 = torch.nn.Linear(2, width, bias=True)
+        self.flatten = torch.nn.Flatten()
+        self.mlp1 = torch.nn.Linear(data_spec.input_shape.numel(), width, bias=True)
         self.mlp2 = torch.nn.Linear(width, width, bias=True)
         self.mlp3 = torch.nn.Linear(width, data_spec.output_shape[-1], bias=True)
 
@@ -29,4 +30,10 @@ class MLPClass(torch.nn.Module):
         tout = y
         # tout = tout.reshape(x.shape[0], 2, -1)
         # return torch.sigmoid(tout)
-        return torch.softmax(tout, dim=-1)
+        return tout  # torch.softmax(tout, dim=-1)
+
+    def forward_full(self, x):
+        return self.output_to_value(self.forward(x))
+
+    def output_to_value(self, output):
+        return torch.softmax(output, dim=-1)

lib/models/transformer.py
@@ -1,6 +1,7 @@
 import torch
 from dataclasses import dataclass
 from lib.dataspec import DataSpec
+import math
 
 
 @dataclass(frozen=True)
@@ -20,9 +21,10 @@ class Transformer(torch.nn.Module):
     def __init__(self, config: TransformerConfig, data_spec: DataSpec):
         super().__init__()
         embed_d = config.embed_d
-        self.embed = torch.nn.Linear(1, embed_d, bias=True)
+        self.embed = torch.nn.Linear(14 * 14, embed_d, bias=True)
         lnorm = torch.nn.LayerNorm(embed_d, eps=1e-5)
 
+        self.pos_embed = PositionalEncoding(config.embed_d, dropout=0.0)
         self.layer = torch.nn.TransformerDecoderLayer(
             d_model=embed_d,
             nhead=config.num_heads,
@@ -44,6 +46,35 @@ class Transformer(torch.nn.Module):
         )
 
     def forward(self, x):
-        embed = self.embed(x)
+        embed = self.embed(x) * math.sqrt(32)
+        embed = self.pos_embed(embed)
         tout = self.transformer(embed, self.mem)
-        return torch.softmax(self.debed(tout), dim=-1)[:, 0, :]
+        # return torch.softmax(self.debed(tout), dim=-1)[:, 0, :]
+        return self.debed(tout[:, 0, :])
+
+    def output_to_value(self, output):
+        return torch.softmax(output, dim=-1)
+
+    def forward_full(self, x):
+        return self.output_to_value(self.forward(x))
+
+
+class PositionalEncoding(torch.nn.Module):
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = torch.nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10.0) / d_model))
+        pe = torch.zeros(1, max_len, d_model)
+        pe[0, :, 0::2] = torch.sin(position * div_term)
+        pe[0, :, 1::2] = torch.cos(position * div_term)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Arguments:
+            x: Tensor, shape ``[seq_len, batch_size, embedding_dim]``
+        """
+        x = x + self.pe[:, : x.size(1)]
+        return self.dropout(x)

lib/train.py
@@ -46,7 +46,8 @@ def validate(
             output = model(input)
 
             metric_sample = MetricSample(
-                prediction=output,
+                output=output,
+                prediction=model.output_to_value(output),
                 target=target,
                 sample_id=sample_id,
                 epoch=train_epoch_state.epoch,
@@ -73,18 +74,19 @@ def train(train_epoch_state: TrainEpochState, train_epoch_spec: TrainEpochSpec):
         optimizer.zero_grad()
         loss_val.backward()
         optimizer.step()
-        # print(f"{loss_val.detach().cpu()}")
+
+        if i == 0:
+            train_epoch_state.memory_stats = torch.cuda.memory_stats_as_nested_dict()
 
         metric_sample = MetricSample(
-            prediction=output,
+            output=output,
+            prediction=model.output_to_value(output),
             target=target,
             sample_id=sample_id,
             epoch=train_epoch_state.epoch,
         )
         for metric in train_epoch_state.train_metrics:
             metric(metric_sample)
-
-    train_epoch_state.epoch += 1
 
 
 def get_checkpoint_path(train_config) -> (Path, Path):
@@ -160,7 +162,7 @@ def deserialize(config: DeserializeConfig):
     val_dataloader = torch.utils.data.DataLoader(
         val_ds,
         batch_size=train_config.batch_size,
-        shuffle=True,
+        shuffle=False,
         drop_last=True,
     )
 
@@ -212,13 +214,13 @@ def create_initial_state(
     )
     val_ds = datasets.create(train_config.val_data_config)
     val_dataloader = torch.utils.data.DataLoader(
-        val_ds, batch_size=train_config.batch_size, shuffle=True, drop_last=True
+        val_ds, batch_size=train_config.batch_size, shuffle=False, drop_last=True
     )
 
     torch.manual_seed(train_config.ensemble_id)
     model = models.create(train_config.model_config, train_ds.data_spec())
     model = model.to(torch.device(device_id))
-    opt = torch.optim.Adam(model.parameters())
+    opt = torch.optim.Adam(model.parameters(), **train_config.optimizer.kwargs)
     train_metrics = [metric() for metric in train_run.train_eval.train_metrics]
     validation_metrics = [
         metric() for metric in train_run.train_eval.validation_metrics
@@ -272,17 +274,15 @@ def do_training(train_run: TrainRun, state: TrainEpochState, device_id):
     )
 
     while state.epoch <= train_run.epochs:
-        # print(f"{state.epoch}")
         train(state, train_epoch_spec)
         validate(state, train_epoch_spec, train_run)
-        # if state.epoch == 20:
-        #     breakpoint()
+        state.epoch += 1
         serialize(serialize_config)
         try:
             now = time.time()
             if now > next_visualization:
                 next_visualization = now + 1
-                visualize_progress(state, train_run)
+                visualize_progress(state, train_run, device_id)
         except Exception as e:
             logging.error("Visualization failed")
             logging.error(str(e))
@@ -293,38 +293,47 @@ def do_training(train_run: TrainRun, state: TrainEpochState, device_id):
     df.to_pickle(path=f"{get_checkpoint_path(train_run.train_config)[0]}.df.pickle")
 
 
-def visualize_progress(state, train_run):
+def visualize_progress(state, train_run, device):
     plt.clt()
     plt.cld()
     plt.scatter()
     epochs = list(range(state.epoch))
-    n_metrics = min(4, len(state.validation_metrics))
     # Two columns
-    plt.subplots(1, 2)
+    plt.subplots(1, 3)
+
+    train_metric_names = [metric.name() for metric in state.train_metrics]
+    val_metric_names = [metric.name() for metric in state.validation_metrics]
+
+    common_metrics = list(set(train_metric_names).intersection(set(val_metric_names)))
+    common_metrics = sorted(common_metrics)
+    n_metrics = min(4, len(common_metrics))
+
+    train_indices = [train_metric_names.index(name) for name in common_metrics]
+    val_indices = [val_metric_names.index(name) for name in common_metrics]
 
     # First column (many metrics in rows)
     plt.subplot(1, 1).subplots(n_metrics, 1)
+
     for idx in range(n_metrics):
-        train_means = [
-            (epoch, state.train_metrics[idx].mean(epoch)) for epoch in epochs
-        ]
+        train_metric = state.train_metrics[train_indices[idx]]
+        val_metric = state.validation_metrics[val_indices[idx]]
+
+        train_means = [(epoch, train_metric.mean(epoch)) for epoch in epochs]
         train_means = [
             (epoch, mean) for (epoch, mean) in train_means if mean is not None
         ]
-        val_means = [
-            (epoch, state.validation_metrics[idx].mean(epoch)) for epoch in epochs
-        ]
+        val_means = [(epoch, val_metric.mean(epoch)) for epoch in epochs]
         val_means = [(epoch, mean) for (epoch, mean) in val_means if mean is not None]
         plt.subplot(1, 1).subplot(idx + 1, 1)
-        plt.title(state.validation_metrics[idx].name())
+        plt.title(common_metrics[idx])
         if len(train_means) > 0:
             x = [epoch for (epoch, mean) in train_means]
             y = [mean for (epoch, mean) in train_means]
-            plt.plot(x, y, label=f"Train {state.validation_metrics[idx].name()}")
+            plt.plot(x, y, label=f"Train {common_metrics[idx]}")
         if len(val_means) > 0:
             x = [epoch for (epoch, mean) in val_means]
             y = [mean for (epoch, mean) in val_means]
-            plt.plot(x, y, label=f"Val {state.validation_metrics[idx].name()}")
+            plt.plot(x, y, label=f"Val {common_metrics[idx]}")
 
     # Second column (config)
     plt.subplot(1, 2).subplots(2, 1)
@@ -336,7 +345,16 @@ def visualize_progress(state, train_run):
     tc = "\n".join(text_config(asdict(train_run)))
     plt.xlim(0, 1)
     plt.ylim(0, 1)
-    plt.text(tc, 0, 1)
+    plt.text(tc, 0, 1, color="black")
+
+    # Third column
+    plt.subplot(1, 3)
+    plot_memory_stats(plt, filter_memory_stats(state.memory_stats), device)
+    # tc = "\n".join(text_config(filter_memory_stats(state.memory_stats)))
+    # plt.xlim(0, 1)
+    # plt.ylim(0, 1)
+    # plt.text(tc, 0, 1, color="black")
+
     plt.show()
 
     checkpoint_path, _ = get_checkpoint_path(train_run.train_config)
@@ -359,3 +377,28 @@ def text_config(config, level=0, y=0):
         else:
             text.append(f"{'  '*level}{key}: {value}")
     return text
+
+
+def filter_memory_stats(memory_stats: dict):
+    return {
+        key: value["all"]
+        for key, value in memory_stats.items()
+        if isinstance(value, dict)
+        and "all" in value
+        and key in ["allocated_bytes", "reserved_bytes", "active_bytes"]
+    }
+
+
+def plot_memory_stats(plt, memory_stats: dict, device):
+    device_stats = torch.cuda.get_device_properties(device)
+
+    def bytes_to_mb(bytes):
+        return bytes / 1e6
+
+    keys = list(memory_stats.keys())
+    current = [bytes_to_mb(memory_stats[key]["current"]) for key in keys]
+    peak = [bytes_to_mb(memory_stats[key]["peak"]) for key in keys]
+    max = [bytes_to_mb(device_stats.total_memory) for key in keys]
+    plt.stacked_bar(
+        keys, [current, peak, max], label=["current", "peak", "max"], orientation="v"
+    )

flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
   outputs = { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
@@ -17,9 +17,10 @@
         propagatedBuildInputs = with pkgs.python3Packages; [
           numpy
           pytorch
+          torchvision
           torchmetrics
           plotext
-          gitpython
+#          gitpython
           wandb
           pandas
           psycopg
@@ -41,12 +42,13 @@
             p.python-lsp-server
             p.numpy
             p.pytorch
+            p.torchvision
             p.plotext
             p.torchmetrics
             p.ipython
             p.black
             p.flake8
-            p.gitpython
+           # p.gitpython
             p.wandb
             p.snakeviz
             p.pandas

lib/classification_metrics.py
@@ -9,10 +9,14 @@ def loss(preds, target):
     return torch.nn.functional.cross_entropy(preds, target, reduction="none")
 
 
-def calibration_error(preds, target, n_classes):
-    return tm.functional.classification.calibration_error(
-        preds, target, n_bins=15, num_classes=n_classes, task="multiclass"
-    )
+# def calibration_error(preds, target, n_classes):
+#     return tm.functional.classification.calibration_error(
+#         preds, target, n_bins=15, num_classes=n_classes, task="multiclass"
+#     )
+
+
+def calibration_error(preds, target):
+    return tm.functional.classification.calibration_error(preds, target, n_bins=15)
 
 
 def create_classification_metrics(data_visualizer, n_classes):
@@ -20,18 +24,22 @@ def create_classification_metrics(data_visualizer, n_classes):
         train_metrics=[
             lambda: Metric(
                 tm.functional.accuracy,
-                metric_kwargs=dict(task="multiclass", num_classes=n_classes),
+                # metric_kwargs=dict(task="multiclass", num_classes=n_classes),
+                metric_kwargs=dict(num_classes=n_classes),
             ),
-            lambda: Metric(loss),
-            lambda: Metric(calibration_error, metric_kwargs=dict(n_classes=n_classes)),
+            lambda: Metric(loss, raw_output=True),
+            # lambda: Metric(calibration_error, metric_kwargs=dict(n_classes=n_classes)),
+            lambda: Metric(calibration_error),
         ],
         validation_metrics=[
             lambda: Metric(
                 tm.functional.accuracy,
-                metric_kwargs=dict(task="multiclass", num_classes=n_classes),
+                # metric_kwargs=dict(task="multiclass", num_classes=n_classes),
+                metric_kwargs=dict(num_classes=n_classes),
             ),
-            lambda: Metric(calibration_error, metric_kwargs=dict(n_classes=n_classes)),
-            lambda: Metric(loss),
+            # lambda: Metric(calibration_error, metric_kwargs=dict(n_classes=n_classes)),
+            lambda: Metric(calibration_error),
+            lambda: Metric(loss, raw_output=True),
         ],
         data_visualizer=data_visualizer,
     )

lib/data.py
@@ -2,6 +2,7 @@ import torch
 from lib.datasets.sine import DataSineConfig, DataSine
 from lib.datasets.spiral import DataSpiralsConfig, DataSpirals
 from lib.datasets.uniform import DataUniformConfig, DataUniform
+from lib.datasets.mnist import DataMNISTConfig, DataMNIST
 
 
 class DataFactory:
@@ -10,6 +11,7 @@ class DataFactory:
         self.datasets[DataSpiralsConfig] = DataSpirals
         self.datasets[DataSineConfig] = DataSine
         self.datasets[DataUniformConfig] = DataUniform
+        self.datasets[DataMNISTConfig] = DataMNIST
 
     def register(self, config_class, data_class):
         self.datasets[config_class] = data_class

lib/metric.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass
-from copy import deepcopy
 from collections.abc import Callable
 import torch
 
 
 @dataclass
 class MetricSample:
+    output: torch.Tensor
     prediction: torch.Tensor
     target: torch.Tensor
     sample_id: torch.Tensor
@@ -24,7 +24,9 @@ class Metric:
         self,
         metric_fn: Callable[[torch.Tensor, torch.Tensor], object],
         metric_kwargs=None,
+        raw_output=False,
     ):
+        self.raw_output = raw_output
         self.values = dict()
         self.metric_fn = metric_fn
         self.metric_name = metric_fn.__name__
@@ -32,14 +34,22 @@ class Metric:
         self.mean_mem = dict()
 
     def __call__(self, metric_sample: MetricSample):
-        prediction = metric_sample.prediction
-        target = metric_sample.target
-        sample_id = metric_sample.sample_id
-        values = (
-            self.metric_fn(preds=prediction, target=target, **self.metric_kwargs)
-            .detach()
-            .cpu()
-        )
+        output = metric_sample.output.detach()
+        prediction = metric_sample.prediction.detach()
+        target = metric_sample.target.detach()
+        sample_id = metric_sample.sample_id.detach()
+        if self.raw_output:
+            values = (
+                self.metric_fn(preds=output, target=target, **self.metric_kwargs)
+                .detach()
+                .cpu()
+            )
+        else:
+            values = (
+                self.metric_fn(preds=prediction, target=target, **self.metric_kwargs)
+                .detach()
+                .cpu()
+            )
         key = MetricSampleKey(sample_id=sample_id, epoch=metric_sample.epoch)
         self.values[key] = values.mean()
 

lib/train_dataclasses.py
@@ -1,5 +1,6 @@
 import torch
 from dataclasses import dataclass
+from dataclasses import field
 from typing import List
 from typing import Callable
 
@@ -18,6 +19,7 @@ class TrainEpochState:
     train_dataloader: torch.utils.data.DataLoader
     epoch: int
     val_dataloader: torch.utils.data.DataLoader = None
+    memory_stats: dict = field(default_factory=torch.cuda.memory_stats_as_nested_dict)
 
 
 @dataclass

commit 1871ad5364a684b7b81262f62ada056ad289d6c9

Fix weight decay bug
Fixes regression introduced in 3ebc5872cbd3c4c6f1a76c3999b55925bbe210bb
where the optimizer kwargs was finally passed along
which suddenly applied the weight decay=0.01 that had been in many
configs before (but unused).
The high weight decay value prevented the models to fit
to the spirals dataset.

Fix MNIST dataspec for generic transformer constructor.

experiments/embed_d/spiral_embed_d.py
@@ -4,14 +4,13 @@ import torchmetrics as tm
 from pathlib import Path
 
 from lib.train_dataclasses import TrainConfig
-from lib.train_dataclasses import TrainEval
 from lib.train_dataclasses import TrainRun
 from lib.train_dataclasses import OptimizerConfig
-from lib.metric import Metric
 from lib.models.transformer import TransformerConfig
-from lib.data import DataSpiralsConfig
+from lib.data_factory import DataSpiralsConfig
 from lib.datasets.spiral_visualization import visualize_spiral
 from lib.generic_ablation import generic_ablation
+from lib.classification_metrics import create_classification_metrics
 
 
 def loss(preds, target):
@@ -27,36 +26,23 @@ def bce(preds, target):
 def create_config(embed_d, ensemble_id):
     train_config = TrainConfig(
         model_config=TransformerConfig(
-            embed_d=embed_d, mlp_dim=10, n_seq=2, batch_size=500
+            embed_d=embed_d,
+            mlp_dim=10,
+            n_seq=2,
+            batch_size=500,
+            num_layers=2,
+            num_heads=1,
         ),
         train_data_config=DataSpiralsConfig(seed=0, N=1000),
         val_data_config=DataSpiralsConfig(seed=1, N=500),
         optimizer=OptimizerConfig(
-            optimizer=torch.optim.Adam, kwargs=dict(weight_decay=0.01)
+            optimizer=torch.optim.Adam, kwargs=dict(weight_decay=0.0001)
         ),
-        loss=torch.nn.BCELoss(),
+        loss=torch.nn.CrossEntropyLoss(),
         batch_size=500,
         ensemble_id=ensemble_id,
     )
-    train_eval = TrainEval(
-        train_metrics=[
-            lambda: Metric(
-                tm.functional.accuracy,
-                metric_kwargs=dict(task="binary", multidim_average="samplewise"),
-            ),
-            lambda: Metric(bce),
-            lambda: Metric(loss),
-        ],
-        validation_metrics=[
-            lambda: Metric(
-                tm.functional.accuracy,
-                metric_kwargs=dict(task="binary", multidim_average="samplewise"),
-            ),
-            lambda: Metric(bce),
-            lambda: Metric(loss),
-        ],
-        data_visualizer=visualize_spiral,
-    )
+    train_eval = create_classification_metrics(visualize_spiral, 2)
     train_run = TrainRun(
         train_config=train_config,
         train_eval=train_eval,
@@ -68,7 +54,7 @@ def create_config(embed_d, ensemble_id):
 
 
 def create_values():
-    return dict(embed_d=[1, 5, 20, 50, 100], ensemble_id=list(range(5)))
+    return dict(embed_d=[100, 1, 5, 20, 50, 100], ensemble_id=list(range(5)))
 
 
 if __name__ == "__main__":

experiments/layers/layers.py
@@ -10,7 +10,8 @@ from lib.train_dataclasses import OptimizerConfig
 from lib.metric import Metric
 from lib.models.transformer import TransformerConfig
 from lib.data import DataSpiralsConfig
-from lib.ablation import ablation
+from lib.datasets.spiral_visualization import visualize_spiral
+from lib.generic_ablation import generic_ablation
 
 
 def loss(preds, target):
@@ -23,26 +24,27 @@ def bce(preds, target):
     )
 
 
-def create_config(embed_d):
+def create_config(layers, ensemble_id):
     train_config = TrainConfig(
         model_config=TransformerConfig(
-            embed_d=embed_d,
-            mlp_dim=1,
+            embed_d=20,
+            mlp_dim=10,
             n_seq=2,
             batch_size=500,
-            num_layers=2,
+            num_layers=layers,
             num_heads=1,
         ),
-        data_config=DataSpiralsConfig(),
+        train_data_config=DataSpiralsConfig(seed=0, N=1000),
+        val_data_config=DataSpiralsConfig(seed=1, N=500),
         optimizer=OptimizerConfig(
             optimizer=torch.optim.Adam, kwargs=dict(weight_decay=0.0001)
         ),
         loss=torch.nn.BCELoss(),
         batch_size=500,
-        ensemble_id=0,
+        ensemble_id=ensemble_id,
     )
     train_eval = TrainEval(
-        metrics=[
+        train_metrics=[
             lambda: Metric(
                 tm.functional.accuracy,
                 metric_kwargs=dict(task="binary", multidim_average="samplewise"),
@@ -50,19 +52,29 @@ def create_config(embed_d):
             lambda: Metric(bce),
             lambda: Metric(loss),
         ],
+        validation_metrics=[
+            lambda: Metric(
+                tm.functional.accuracy,
+                metric_kwargs=dict(task="binary", multidim_average="samplewise"),
+            ),
+            lambda: Metric(bce),
+            lambda: Metric(loss),
+        ],
+        data_visualizer=visualize_spiral,
     )
     train_run = TrainRun(
         train_config=train_config,
         train_eval=train_eval,
         epochs=500,
         save_nth_epoch=20,
+        validate_nth_epoch=20,
     )
     return train_run
 
 
 def create_values():
-    return [1, 5, 20, 50, 100]
+    return dict(layers=[1, 2, 3, 4], ensemble_id=list(range(3)))
 
 
 if __name__ == "__main__":
-    ablation(Path(__file__).parent / "results", create_config, create_values())
+    generic_ablation(Path(__file__).parent / "results", create_config, create_values())

experiments/mlp/spiral.py
@@ -19,7 +19,7 @@ def create_config(mlp_dim, ensemble_id):
         val_data_config=DataSpiralsConfig(seed=1, N=500),
         loss=torch.nn.CrossEntropyLoss(),
         optimizer=OptimizerConfig(
-            optimizer=torch.optim.Adam, kwargs=dict(weight_decay=0.01)
+            optimizer=torch.optim.Adam, kwargs=dict(weight_decay=0.0001)
         ),
         batch_size=500,
         ensemble_id=ensemble_id,
@@ -39,5 +39,5 @@ if __name__ == "__main__":
     generic_ablation(
         Path(__file__).parent / "results",
         create_config,
-        dict(mlp_dim=[100, 10, 50, 100, 200], ensemble_id=list(range(5))),
+        dict(mlp_dim=[200, 10, 50, 100, 200], ensemble_id=list(range(5))),
     )

experiments/mlp_dim/spiral_mlp_dim.py
@@ -9,7 +9,7 @@ from lib.train_dataclasses import TrainRun
 from lib.train_dataclasses import OptimizerConfig
 from lib.metric import Metric
 from lib.models.transformer import TransformerConfig
-from lib.data import DataSpiralsConfig
+from lib.data_factory import DataSpiralsConfig
 from lib.datasets.spiral_visualization import visualize_spiral
 from lib.generic_ablation import generic_ablation
 
@@ -27,10 +27,15 @@ def bce(preds, target):
 def create_config(mlp_dim, ensemble_id):
     train_config = TrainConfig(
         model_config=TransformerConfig(
-            embed_d=50, mlp_dim=mlp_dim, n_seq=2, batch_size=500
+            embed_d=50,
+            mlp_dim=mlp_dim,
+            n_seq=2,
+            batch_size=500,
+            num_layers=1,
+            num_heads=1,
         ),
         optimizer=OptimizerConfig(
-            optimizer=torch.optim.Adam, kwargs=dict(weight_decay=0.01)
+            optimizer=torch.optim.Adam, kwargs=dict(weight_decay=0.0001)
         ),
         train_data_config=DataSpiralsConfig(seed=0, N=1000),
         val_data_config=DataSpiralsConfig(seed=1, N=500),

experiments/uncertainty/spiral.py
@@ -26,7 +26,7 @@ def create_config(ensemble_id):
         val_data_config=DataSpiralsConfig(seed=1, N=500),
         loss=torch.nn.CrossEntropyLoss(),
         optimizer=OptimizerConfig(
-            optimizer=torch.optim.Adam, kwargs=dict(weight_decay=0.01)
+            optimizer=torch.optim.Adam, kwargs=dict(weight_decay=0.0001)
         ),
         batch_size=500,
         ensemble_id=ensemble_id,

lib/datasets/mnist.py
@@ -26,7 +26,7 @@ class DataMNIST(torch.utils.data.Dataset):
 
     def data_spec(self):
         return DataSpec(
-            input_shape=torch.Size([28 * 28]),
+            input_shape=torch.Size([4, 14 * 14]),
             target_shape=torch.Size([1]),
             output_shape=torch.Size([self.n_classes]),
         )

lib/datasets/mnist_visualization.py
@@ -8,7 +8,7 @@ def visualize_mnist(plt, state: TrainEpochState):
     input, target, ids = next(iter(state.val_dataloader))
     with torch.no_grad():
         input = input.to("cuda:0", non_blocking=True)
-        output = state.model(input).cpu()
+        output = state.model(input)[1].cpu()
         digits = torch.argmax(output, dim=-1)
 
     images = input.cpu().reshape(-1, 1, 28, 28)

lib/models/mlp.py
@@ -30,10 +30,10 @@ class MLPClass(torch.nn.Module):
         tout = y
         # tout = tout.reshape(x.shape[0], 2, -1)
         # return torch.sigmoid(tout)
-        return tout, torch.softmax(tout, dim=-1)
+        return tout, torch.softmax(tout.detach(), dim=-1)
 
-    # def forward_full(self, x):
-    #     return self.output_to_value(self.forward(x))
+    def forward_full(self, x):
+        return self.output_to_value(self.forward(x))
 
-    # def output_to_value(self, output):
-    #     return torch.softmax(output, dim=-1)
+    def output_to_value(self, output):
+        return torch.softmax(output, dim=-1)

lib/models/transformer.py
@@ -21,7 +21,7 @@ class Transformer(torch.nn.Module):
     def __init__(self, config: TransformerConfig, data_spec: DataSpec):
         super().__init__()
         embed_d = config.embed_d
-        self.embed = torch.nn.Linear(14 * 14, embed_d, bias=True)
+        self.embed = torch.nn.Linear(data_spec.input_shape[-1], embed_d, bias=True)
         lnorm = torch.nn.LayerNorm(embed_d, eps=1e-5)
 
         self.pos_embed = PositionalEncoding(config.embed_d, dropout=0.0)
@@ -51,7 +51,7 @@ class Transformer(torch.nn.Module):
         tout = self.transformer(embed, self.mem)
         # return torch.softmax(self.debed(tout), dim=-1)[:, 0, :]
         output = self.debed(tout[:, 0, :])
-        return output, self.output_to_valueout(output)
+        return output, self.output_to_value(output)
 
     def output_to_value(self, output):
         return torch.softmax(output, dim=-1)

lib/train.py
@@ -22,8 +22,6 @@ from lib.train_dataclasses import TrainEpochState
 from lib.train_dataclasses import TrainEpochSpec
 from lib.train_dataclasses import TrainRun
 
-from lib.ddp import ddp_setup
-
 
 def validate(
     train_epoch_state: TrainEpochState,
@@ -156,7 +154,6 @@ def deserialize(config: DeserializeConfig):
     train_dataloader = torch.utils.data.DataLoader(
         train_ds,
         batch_size=train_config.batch_size,
-        shuffle=False,
         drop_last=True,
         sampler=torch.utils.data.distributed.DistributedSampler(train_ds),
     )
@@ -172,11 +169,11 @@ def deserialize(config: DeserializeConfig):
     model = model_factory.get_factory().create(
         train_config.model_config, val_ds.data_spec()
     )
-    model.load_state_dict(data_dict["model"])
     model = model.to(torch.device(config.device_id))
     model = torch.nn.parallel.DistributedDataParallel(
-        model, device_ids=[config.device_id]
+        model, device_ids=[config.device_id], find_unused_parameters=True
     )
+    model.load_state_dict(data_dict["model"])
 
     optimizer = train_config.optimizer.optimizer(
         model.parameters(), **train_config.optimizer.kwargs
@@ -216,7 +213,6 @@ def create_initial_state(train_run: TrainRun, device_id):
     train_dataloader = torch.utils.data.DataLoader(
         train_ds,
         batch_size=train_config.batch_size,
-        shuffle=False,
         drop_last=True,
         sampler=torch.utils.data.distributed.DistributedSampler(train_ds),
     )
@@ -235,7 +231,7 @@ def create_initial_state(train_run: TrainRun, device_id):
     )
     init_model = init_model.to(torch.device(device_id))
     init_model = torch.nn.parallel.DistributedDataParallel(
-        init_model, device_ids=[device_id]
+        init_model, device_ids=[device_id], find_unused_parameters=True
     )
     opt = torch.optim.Adam(init_model.parameters(), **train_config.optimizer.kwargs)
     train_metrics = [metric() for metric in train_run.train_eval.train_metrics]

lib/uncertainty.py
@@ -84,8 +84,6 @@ def uncertainty(data_loader: torch.utils.data.DataLoader, ensemble: Ensemble, de
         for idx, member in enumerate(ensemble.members):
             probs[:, idx, :] = member(input)[1].detach()
 
-        # breakpoint()
-
         MI = mutual_information(probs)
         H = predictive_entropy(probs)
         A = mean_entropy(probs)