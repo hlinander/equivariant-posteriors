be4336a60ec418c5b93762edd5cf400539180478
Hampus Linander <hampus.linander@pm.me>
2023-06-13 23:30:57 +0200

    Fix stable hash for optimizer
    Include optimizer name and config in hash

--- lib/stable_hash.py
+++ lib/stable_hash.py
@@ -7,21 +7,22 @@ def json_default(data):
     try:
         return {"__type": type(data).__name__, **dataclasses.asdict(data)}
     except:
-        return type(data).__name__
+        try:
+            return data.__name__
+        except:
+            return type(data).__name__
+
+
+def json_dumps_dataclass(data_class):
+    return json.dumps(
+        data_class,
+        default=json_default,
+        ensure_ascii=False,
+        sort_keys=True,
+        indent=None,
+        separators=(",", ":"),
+    ).encode("utf-8")
 
 
 def stable_hash(data_class):
-    return (
-        hashlib.md5(
-            json.dumps(
-                data_class,
-                default=json_default,
-                ensure_ascii=False,
-                sort_keys=True,
-                indent=None,
-                separators=(",", ":"),
-            ).encode("utf-8")
-        )
-        .digest()
-        .hex()
-    )
+    return hashlib.md5(json_dumps_dataclass(data_class)).digest().hex()
