b042fb34e361d07ae495f73d4a0fa799c02381c5
Hampus Linander <hampus.linander@gu.se>
2023-09-07 14:22:43 +0200

    Assign random port with torchrun

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 set -x
 export PYTHONPATH=$(pwd)
 ulimit -n 64000
-torchrun --standalone --nnodes=1 --nproc_per_node=1 $@
+torchrun --nnodes=1 --nproc_per_node=1 --rdzv_backend=c10d --rdzv_endpoint=localhost:0 $@
