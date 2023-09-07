6830c1297498a5d4ab57a72c7876a6c87d0b445e
Hampus Linander <hampus.linander@gu.se>
2023-09-07 13:30:38 +0200

    Fix SLURM task id extraction

--- lib/slurm.py
+++ lib/slurm.py
@@ -3,7 +3,10 @@ import os
 
 def get_task_id():
     if "SLURM_ARRAY_TASK_ID" in os.environ:
-        return int(os.environ.get("SLURM_ARRAY_TASK_ID"))
+        try:
+            return int(os.environ.get("SLURM_ARRAY_TASK_ID"))
+        except ValueError:
+            return None
     else:
         return None
         # raise Exception("SLURM_ARRAY_TASK_ID not set!")
