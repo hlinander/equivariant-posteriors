6462a70a18df204fb5c2f6b7e72ef78c9f120a40
Hampus Linander <hampus.linander@pm.me>
2023-09-09 12:06:54 +0200

    Refactor training locks to separate path
    Enables locks in scenarios where checkpoints are sstored
    on e.g. a network filesystem.

--- lib/paths.py
+++ lib/paths.py
@@ -8,6 +8,14 @@ def get_or_create_checkpoint_path(train_config) -> Path:
     return checkpoint
 
 
+def get_lock_path(train_config) -> Path:
+    config_hash = stable_hash(train_config)
+    lock_dir = Path("locks/")
+    lock_dir.mkdir(exist_ok=True, parents=True)
+    lock_path = lock_dir / f"lock_{config_hash}"
+    return lock_path
+
+
 def get_checkpoint_path(train_config) -> Path:
     config_hash = stable_hash(train_config)
     checkpoint_dir = Path("checkpoints/")
