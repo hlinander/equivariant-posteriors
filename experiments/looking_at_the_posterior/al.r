c8d64dba6363324364537f3de6277232a430b5cc
Hampus Linander <hampus.linander@pm.me>
2023-09-23 20:30:34 +0200

    Add calibrated uncertainty with true negative weighting
    Picks samples from bins with highest expected true negative rate.

--- experiments/looking_at_the_posterior/al.r
+++ experiments/looking_at_the_posterior/al.r
@@ -21,21 +21,62 @@ df <- dbGetQuery(con, query)
 
 # Close the database connection
 dbDisconnect(con)
-df1 <- read.csv("./alvis/al_33fd470/calibrated_uncertainty.csv")
-df2 <- read.csv("./alvis/al_33fd470/random.csv")
-df <- rbind(df1, df2)
-ggplot(df, aes(x=fraction, y=value, group=model, color=model)) + geom_line() + facet_wrap(~metric,scales="free_y")
+df1 <- read.csv("./alvis/al_8f9d209/conv_predictive_entropy.csv")
+df2 <- read.csv("./alvis/al_3a5abfa/conv_calibrated_uq.csv")
+df3 <- read.csv("./alvis/al_b8b383b/conv_mutual_information.csv")
+df4 <- read.csv("./alvis/al_b974f7f/conv_random.csv")
+df <- rbind(df1, df2, df3, df4)
+p<-(ggplot(df, aes(x=fraction, y=value, group=aquisition, color=aquisition)) + geom_line(aes(linetype=aquisition)) + 
+    facet_grid(rows=vars(metric),scales="free_y") +
+    theme_minimal())
+ggsave("./experiments/looking_at_the_posterior/conv_al.pdf", p)
+
+df_cal<-read.csv("./alvis/al_3a5abfa/uq_calibration_step_005_calibrated_uq.csv")
+df_aq <-read.csv("./alvis/al_3a5abfa/uq_aquired_step_005_calibrated_uq.csv")
+df_pool <-read.csv("./alvis/al_3a5abfa/uq_pool_step_005_calibrated_uq.csv")
+min_samples_mean <- function(z) {
+  if (length(z) >= 10) {
+    return(length(z))
+    return(mean(z))
+  } else {
+    return(NA)  # return NA for bins with less than 10 samples
+  }
+}
+
+(ggplot() +
+    stat_summary_2d(data=df_cal, aes(x=H, y=MI, z=accuracy, fill=..value..), fun=min_samples_mean, bins=15) +
+    geom_point(data=df_aq, aes(x=H, y=MI), color="red", fill="green", shape=19, size=5) +
+    geom_point(data=df_pool, aes(x=H, y=MI), color="red", shape=2) +
+    scale_fill_viridis_c() +
+    theme_minimal(base_size=40) #+ 
+  #scale_x_continuous(trans = log_trans()) +
+  #scale_y_continuous(trans = log_trans())
+)
+
+N, acc
+N_tn = N * (1 - acc)
+
+
+df1 <- read.csv("./alvis/al_dace34e/calibrated_uq.csv")
+df2 <- read.csv("./alvis/al_ad86f40/random.csv")
+df3 <- read.csv("./alvis/al_3dec3eb/predictive_entropy.csv")
+df4 <- read.csv("./alvis/al_c402cb3/mutual_information.csv")
+df <- rbind(df1, df2, df3, df4)
+p <- (ggplot(df, aes(x=fraction, y=value, group=model, color=model)) + geom_line(aes(linetype=model)) + 
+    facet_grid(~metric,scales="free_y") +
+    theme_minimal())
+ggsave("./experiments/looking_at_the_posterior/conv_al.pdf", p)
+
 df <- read.csv("./experiments/looking_at_the_posterior/al_794843e/uq_calibration.csv")
 p <- (ggplot(df, aes(x = fraction, y = value, group = model, color=model)) + 
-        geom_line() + 
+        geom_line(linetype=9) + 
         facet_wrap( ~metric, scales="free_y") + 
-        theme_minimal(base_size = 24) +
-        xlim(0, 1)
+        theme_minimal(base_size = 44))
 print(p)
 
-df_cal<-read.csv("./alvis/al_28ba7eb/uq_calibration_step_006_calibrated_uncertainty.csv")
-df_aq <-read.csv("./alvis/al_28ba7eb/uq_aquired_step_006_calibrated_uncertainty.csv")
-df_pool <-read.csv("./alvis/al_28ba7eb/uq_pool_step_006_calibrated_uncertainty.csv")
+df_cal<-read.csv("./alvis/al_dace34e/uq_calibration_step_005_calibrated_uq.csv")
+df_aq <-read.csv("./alvis/al_dace34e/uq_aquired_step_005_calibrated_uq.csv")
+df_pool <-read.csv("./alvis/al_dace34e/uq_pool_step_005_calibrated_uq.csv")
 np <- import("numpy")
 mean_accs <- np$load("./alvis/al_28ba7eb/uq_mean_acc_step_006_calibrated_uncertainty.npy")
 mean_accs <- pivot_longer()
@@ -52,9 +93,9 @@ stat_summary_2d(data=df_cal, aes(x=H, y=MI, z=accuracy, fill=..value..), fun=min
    geom_point(data=df_aq, aes(x=H, y=MI), color="red", fill="green", shape=19, size=5) +
     geom_point(data=df_pool, aes(x=H, y=MI), color="red", shape=2) +
     scale_fill_viridis_c() +
-    theme_minimal(base_size=40) + 
-    scale_x_continuous(trans = log_trans()) +
-    scale_y_continuous(trans = log_trans())
+    theme_minimal(base_size=40) #+ 
+    #scale_x_continuous(trans = log_trans()) +
+    #scale_y_continuous(trans = log_trans())
   )
 
 (ggplot() +
--- experiments/looking_at_the_posterior/al_aquisition.py
+++ experiments/looking_at_the_posterior/al_aquisition.py
@@ -262,3 +262,169 @@ def al_aquisition_calibrated_uncertainty(
         / f"uq_aquired_step_{al_step.step:03d}_{al_step.al_config.aquisition_method}.csv"
     )
     return al_step.aquired_ids + new_ids
+
+
+@dataclass
+class CalibratedUncertaintyTrueNegativeConfig:
+    bins: int = 20
+
+
+def al_aquisition_calibrated_uncertainty_true_negative(
+    # al_step.al_config: ALConfig,
+    # ensemble: object,
+    # rng: np.random.Generator,
+    al_step: ALStep,
+    config: CalibratedUncertaintyTrueNegativeConfig,
+    output_path: Path,
+    device_id,
+):
+    ds_uq_calibration = data_factory.get_factory().create(
+        al_step.al_config.uq_calibration_data_config
+    )
+    dl_uq_calibration = torch.utils.data.DataLoader(
+        ds_uq_calibration,
+        batch_size=256,
+        shuffle=False,
+        drop_last=False,
+    )
+    ds_pool = data_factory.get_factory().create(al_step.al_config.data_pool_config)
+    dl_pool = torch.utils.data.DataLoader(
+        ds_pool,
+        batch_size=256,
+        shuffle=False,
+        drop_last=False,
+    )
+
+    uq_calibration = uncertainty(dl_uq_calibration, al_step.ensemble, device_id)
+    uq_to_dataframe(uq_calibration).to_csv(
+        output_path
+        / f"uq_calibration_step_{al_step.step:03d}_{al_step.al_config.aquisition_method}.csv"
+    )
+    acc = (
+        torch.where(
+            uq_calibration.targets[:, None].cpu()
+            == uq_calibration.mean_pred[:, None].cpu(),
+            1.0,
+            0.0,
+        )
+        .numpy()
+        .squeeze()
+    )
+    # log_H = torch.log(uq_calibration.H)
+    # log_MI = torch.log(uq_calibration.MI)
+    mean_acc_calibration, x_bins, y_bins, bin_number = binned_statistic_2d(
+        uq_calibration.H.cpu().numpy(),
+        uq_calibration.MI.cpu().numpy(),
+        acc,
+        statistic="mean",
+        bins=config.bins,
+        expand_binnumbers=True,
+    )
+    cal_counts, _, _, _ = binned_statistic_2d(
+        uq_calibration.H.cpu().numpy(),
+        uq_calibration.MI.cpu().numpy(),
+        acc,
+        statistic="count",
+        bins=config.bins,
+        expand_binnumbers=True,
+    )
+
+    # breakpoint()
+    np.save(
+        output_path
+        / f"uq_mean_acc_step_{al_step.step:03d}_{al_step.al_config.aquisition_method}.npy",
+        mean_acc_calibration,
+    )
+    # breakpoint()
+
+    uq_pool = uncertainty(dl_pool, al_step.ensemble, device_id)
+    uq_to_dataframe(uq_pool).to_csv(
+        output_path
+        / f"uq_pool_step_{al_step.step:03d}_{al_step.al_config.aquisition_method}.csv"
+    )
+    acc = (
+        torch.where(
+            uq_pool.targets[:, None].cpu() == uq_pool.mean_pred[:, None].cpu(),
+            1.0,
+            0.0,
+        )
+        .numpy()
+        .squeeze()
+    )
+    # log_H = torch.log(uq_pool.H)
+    # log_MI = torch.log(uq_pool.MI)
+    mean_acc_pool, x_bins, y_bins, bin_number_pool = binned_statistic_2d(
+        uq_pool.H.cpu().numpy(),
+        uq_pool.MI.cpu().numpy(),
+        acc,
+        statistic="mean",
+        bins=[x_bins, y_bins],
+        expand_binnumbers=True,
+    )
+
+    bin_coords = list(zip(bin_number_pool[0] - 1, bin_number_pool[1] - 1))
+    bin_coords_and_sample_ids = list(zip(bin_coords, uq_pool.sample_ids))
+    bin_coords_and_sample_ids_with_values = [
+        (coord, sample_id)
+        for (coord, sample_id) in bin_coords_and_sample_ids
+        if (
+            coord[0] >= 0
+            and coord[0] < len(x_bins) - 1
+            and coord[1] >= 0
+            and coord[1] < len(y_bins) - 1
+        )
+    ]
+
+    just_coords_x = [coord[0] for coord, _ in bin_coords_and_sample_ids_with_values]
+    just_coords_y = [coord[1] for coord, _ in bin_coords_and_sample_ids_with_values]
+    just_sample_ids = [
+        sample_id for _, sample_id in bin_coords_and_sample_ids_with_values
+    ]
+
+    inferred_accs = mean_acc_calibration[just_coords_x, just_coords_y]
+    inferred_count = cal_counts[just_coords_x, just_coords_y]
+    inferred_true_negative = inferred_count * (1.0 - inferred_accs)
+
+    np.save(
+        output_path
+        / f"uq_inferred_acc_step_{al_step.step:03d}_{al_step.al_config.aquisition_method}.npy",
+        inferred_accs,
+    )
+    accs_and_coords_and_ids = list(
+        zip(inferred_true_negative, just_coords_x, just_coords_y, just_sample_ids)
+    )
+
+    def nans_last_in_sort(x):
+        new_acc = x[0]
+        if np.isnan(new_acc):
+            new_acc = 0.0
+        return new_acc, x[1], x[2], x[3]
+
+    accs_and_coords_and_ids_no_nan = list(
+        map(nans_last_in_sort, accs_and_coords_and_ids)
+    )
+
+    sorted_acc = sorted(accs_and_coords_and_ids_no_nan, key=lambda x: -x[0])
+
+    # breakpoint()
+    sorted_ids = [int(sample_idx) for _, _, _, sample_idx in sorted_acc]
+
+    # Restrict to available pool ids
+    sorted_ids = [
+        sample_idx for sample_idx in sorted_ids if sample_idx in al_step.pool_ids
+    ]
+
+    # breakpoint()
+
+    new_ids = sorted_ids[
+        : int(
+            (al_step.al_config.n_end - al_step.al_config.n_start)
+            / al_step.al_config.n_steps
+        )
+    ]
+    df_uq_pool = uq_to_dataframe(uq_pool)
+    df_uq_pool[df_uq_pool["idx"].isin(new_ids)].to_csv(
+        output_path
+        / f"uq_aquired_step_{al_step.step:03d}_{al_step.al_config.aquisition_method}.csv"
+    )
+    return al_step.aquired_ids + new_ids
