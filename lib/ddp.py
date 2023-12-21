5ba51b6f5849ba9e8731f3a369099d42f666b9e7
Hampus Linander <hampus.linander@gu.se>
2023-12-21 18:47:44 +0100

    Support multiple calls to ddp_setup
    Saved current device in singleton

--- lib/ddp.py
+++ lib/ddp.py
@@ -12,19 +12,27 @@ def get_rank() -> int:
         return 0
 
 
+_DEVICE = None
+
+
 def ddp_setup(backend=None) -> str:
     """
     Args:
         rank: Unique identifier of each process
        world_size: Total number of processes
     """
+    global _DEVICE
     # torch.multiprocessing.set_sharing_strategy('file_system')
+    if _DEVICE is not None:
+        return _DEVICE
     if "TORCH_DEVICE" in os.environ:
         device = os.environ["TORCH_DEVICE"]
         print(f"Using device {device}")
+        _DEVICE = device
         return device
 
     if "EP_TORCHRUN" not in os.environ:
+        _DEVICE = "cpu"
         return "cpu"
 
     if backend is None:
@@ -39,8 +47,10 @@ def ddp_setup(backend=None) -> str:
     device_id = int(os.environ["LOCAL_RANK"])
     if backend == "nccl":
         torch.cuda.set_device(device_id)
+        _DEVICE = device_id
         return device_id
     if backend == "gloo":
+        _DEVICE = "cpu"
         return "cpu"
 
     raise Exception("Unsupported backend")
