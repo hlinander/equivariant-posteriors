73786c50aac68e36314e27a2842da1fc95a31c76
Hampus Linander <hampus.linander@pm.me>
2024-08-27 13:03:28 +0200

    [WIP] Update visualizer with duck

--- start_postgres.sh
+++ start_postgres.sh
@@ -14,4 +14,7 @@ host    all             all             0.0.0.0/0               md5
 host    all             all             ::1/128                 md5
 host    replication     postgres        ::1/128                 md5
 EOM
+if ! grep -q timescaledb $PG_PATH/postgresql.conf; then
+  echo "shared_preload_libraries = 'timescaledb'" >> $PG_PATH/postgresql.conf  
+fi
 $(readlink -f $(which postgres)) -D $PG_PATH -k $PG_PATH -i
